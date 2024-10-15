27c1af55a7526dc4cb0a7b2970a13930c071a835
edogola4 <edogola4@gmail.com>
2024-10-15 22:09:43 +0300

    Js control flow

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+if ( someValue <= 400 ){
+  return 'This one is on me!';
+}else if ( someValue > 400 && someValue <= 2000 ){
+  return 'That will be twenty bucks.';
+}else if (someValue > 2000 && someValue <= 2500){
+  return 'I will gladly take your thirty bucks.';
+}else {
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
+/**
+  This function takes city as an argument.
+It uses a ternary operator to check if the city is "NYC".
+If city === 'NYC', it returns 'Ok, sounds good.'.
+Otherwise, it returns 'No go.'.
+ */
+
+function ternaryCheckCity(city){
   // Write your code here!
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+
+/**
+ * Explanation:
+The function takes a tip argument.
+It uses a switch statement to check the value of tip:
+If tip is 'generous', it returns 'Thank you so much.'.
+If tip is 'not as generous', it returns 'Thank you.'.
+For any other value (handled by the default case), it returns 'Bye.'.
+ */
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return "Thank you so much."
+      case 'not as generous':
+        return "Thank you."
+        default:
+          return "Bye."
+  }
 }
